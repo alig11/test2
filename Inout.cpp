a90bdb538bd0bd06afc01a062620d8ec8ba84e3a
Alikhan Khasiev <khasiev.alikhan11@gmail.com>
2018-06-21 19:03:42 +0700

    Second commit

---
+++ Inout.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+
+void FileInput(char *TextMas)
+{
+	std::ifstream ifs("txt.txt");
+	std::string content((std::istreambuf_iterator<char>(ifs)),
+		(std::istreambuf_iterator<char>()));
+	strcpy(TextMas, content.c_str());
+}
